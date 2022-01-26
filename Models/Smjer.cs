0b3254cfcbbe21d8bcd339b6d688024dcaaf210c
Marko <64027072+mdankovic2541@users.noreply.github.com>
2022-01-26 15:56:04 +0100

    Add files via upload

---
+++ Models/Smjer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIByMe.Models
+{
+    public class Smjer
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+        
+    }
+}
