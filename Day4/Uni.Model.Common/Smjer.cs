fff78874708e765b63df19960a2c9416b9db1a20
Marko <64027072+mdankovic2541@users.noreply.github.com>
2022-01-27 15:06:57 +0100

    Add Student and Smjer models

---
+++ Day4/Uni.Model.Common/Smjer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uni.Model.Common;
+
+namespace Uni.Model
+{
+    public class Smjer : ISmjer
+    {
+        public int Id { get; set; }
+
+        public string Naziv { get; set; }
+    }
+}
