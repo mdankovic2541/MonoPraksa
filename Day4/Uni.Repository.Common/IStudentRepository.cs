00505abf9efdd38b7a294b42da1b6d7fa320ed26
Marko <64027072+mdankovic2541@users.noreply.github.com>
2022-01-27 15:09:21 +0100

    Add Student and Smjer repository

---
+++ Day4/Uni.Repository.Common/IStudentRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Uni.Model;
+
+namespace Uni.Repository.Common
+{
+    public interface IStudentRepository
+    {
+        List<Student> GetAll();
+
+
+        List<Student> GetById(int id);
+
+        
+        bool Post(Student smjer);
+
+        
+        bool Put(int id, Student smjer);
+
+
+        bool DeleteById(int id);
+        
+    }
+}
