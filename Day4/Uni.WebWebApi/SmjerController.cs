e4fe3f2031f519e445564c3be3a67b6ab47d4d60
Marko <64027072+mdankovic2541@users.noreply.github.com>
2022-01-27 15:14:20 +0100

    Add Student and Smjer controller

---
+++ Day4/Uni.WebWebApi/SmjerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Uni.Model;
+using Uni.Service;
+using Uni.Service.Common;
+
+namespace Uni.WebApi.Controllers
+{
+
+    public class SmjerController : ApiController
+    {
+        public SmjerController() { }
+
+        protected ISmjerService Service = new SmjerService();
+
+        [HttpGet]
+        public List<Smjer> GetAll()
+        {
+            return Service.GetAll();
+        }
+
+        [HttpGet]
+        public List<Smjer> GetById(int id)
+        {
+            return Service.GetById(id);
+        }
+
+        [HttpPost]
+        public bool Post(Smjer smjer)
+        {
+            return Service.Post(smjer);
+        }
+    
+
+        [HttpPut]
+        public bool Put(int id, Smjer smjer)
+        {
+            return Service.Put(id,smjer);
+        }
+
+        [HttpDelete]
+        public bool DeleteById(int id)
+        {
+            return Service.DeleteById(id);
+        }
+
+    }
+
+}
---
+++ Day4/Uni.WebWebApi/StudentController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Uni.Model;
+using Uni.Service;
+using Uni.Service.Common;
+
+namespace Uni.WebApi.Controllers
+{
+
+
+
+    public class StudentController : ApiController
+    {
+
+
+
+        public StudentController() { }
+        protected IStudentService Service = new StudentService();
+
+        [HttpGet]
+        public List<Student> GetAll()
+        {
+            return Service.GetAll();
+        }
+
+        [HttpGet]
+        public List<Student> GetById(int id)
+        {
+            return Service.GetById(id);
+        }
+
+        [HttpPost]
+        public bool Post(Student smjer)
+        {
+            return Service.Post(smjer);
+        }
+
+
+        [HttpPut]
+        public bool Put(int id, Student smjer)
+        {
+            return Service.Put(id, smjer);
+        }
+
+        [HttpDelete]
+        public bool DeleteById(int id)
+        {
+            return Service.DeleteById(id);
+        }
+
+    }
+}
+    
+
