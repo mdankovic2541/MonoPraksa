0f848807d4814352d9745a849b9ec6daaf626fb6
Marko <64027072+mdankovic2541@users.noreply.github.com>
2022-01-27 15:12:14 +0100

    Add Student and Smjer service

---
+++ Day4/Uni.Service.Common/ISmjerService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Uni.Model;
+
+namespace Uni.Service.Common
+{
+    public interface ISmjerService
+    {
+        List<Smjer> GetAll();
+
+
+
+        List<Smjer> GetById(int id);
+
+
+
+        bool Post(Smjer smjer);
+
+
+
+        bool Put(int id, Smjer smjer);
+
+
+
+        bool DeleteById(int id);
+    }
+}
---
+++ Day4/Uni.Service.Common/IStudentService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uni.Model;
+
+namespace Uni.Service.Common
+{
+    
+    public interface IStudentService
+    {
+        List<Student> GetAll();
+
+
+
+        List<Student> GetById(int id);
+
+
+
+        bool Post(Student smjer);
+
+
+
+        bool Put(int id, Student smjer);
+
+
+
+        bool DeleteById(int id);
+    }
+    
+}
