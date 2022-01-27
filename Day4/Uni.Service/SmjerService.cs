de2f428475e95166854c057558e1dea2dc6382b0
Marko <64027072+mdankovic2541@users.noreply.github.com>
2022-01-27 15:12:53 +0100

    Add Student and Smjer service

---
+++ Day4/Uni.Service/SmjerService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uni.Model;
+using Uni.Repository;
+using Uni.Repository.Common;
+using Uni.Service;
+using Uni.Service.Common;
+
+namespace Uni.Service
+{
+    public class SmjerService : ISmjerService
+    {
+
+       
+        public SmjerService()
+        {
+        }
+
+        protected ISmjerRepository Repository = new SmjerRepository();
+
+        public List<Smjer> GetAll()
+        {
+            return Repository.GetAll();
+        }
+
+        
+        public List<Smjer> GetById(int id)
+        {
+            return Repository.GetById(id);
+        }
+
+        
+        public bool Post(Smjer smjer)
+        {
+            return Repository.Post(smjer);
+        }
+
+
+        
+        public bool Put(int id, Smjer smjer)
+        {
+            return Repository.Put(id, smjer);
+        }
+
+        
+        public bool DeleteById(int id)
+        {
+            return Repository.DeleteById(id);
+        }
+
+    }
+}
---
+++ Day4/Uni.Service/StudentService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Uni.Model;
+using Uni.Repository;
+using Uni.Repository.Common;
+using Uni.Service.Common;
+
+namespace Uni.Service
+{
+    public class StudentService : IStudentService
+    {
+       
+
+        public StudentService()
+        {
+        }
+
+        protected IStudentRepository Repository = new StudentRepository();
+
+        public List<Student> GetAll()
+        {
+            return Repository.GetAll();
+        }
+
+
+        public List<Student> GetById(int id)
+        {
+            return Repository.GetById(id);
+        }
+
+
+        public bool Post(Student smjer)
+        {
+            return Repository.Post(smjer);
+        }
+
+
+
+        public bool Put(int id, Student smjer)
+        {
+            return Repository.Put(id, smjer);
+        }
+
+
+        public bool DeleteById(int id)
+        {
+            return Repository.DeleteById(id);
+        }
+
+    }
+}
