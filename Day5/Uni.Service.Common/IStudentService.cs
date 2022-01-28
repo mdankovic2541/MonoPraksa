b463d6ad850caebc6dc8e25141b3d3082ea977da
Marko <64027072+mdankovic2541@users.noreply.github.com>
2022-01-28 20:07:58 +0100

    Add Service.Common

---
+++ Day5/Uni.Service.Common/IStudentService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uni.Model;
+
+namespace Uni.Service.Common
+{
+    
+    public interface IStudentService
+    {
+        Task<List<Student>> GetAll();
+
+
+
+        Task<Student> GetById(int id);
+
+
+
+        Task<bool> Post(Student smjer);
+
+
+
+        Task<bool> Put(int id, Student smjer);
+
+
+
+        Task<bool> DeleteById(int id);
+    }
+    
+}
