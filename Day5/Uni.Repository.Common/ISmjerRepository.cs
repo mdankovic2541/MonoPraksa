3ea57988e90273ef0c02733db7b295abadab0647
Marko <64027072+mdankovic2541@users.noreply.github.com>
2022-01-28 20:07:07 +0100

    Add Repository and Repository.Common

---
+++ Day5/Uni.Repository.Common/ISmjerRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Uni.Model;
+
+namespace Uni.Repository.Common
+{
+    public interface ISmjerRepository
+    {
+        Task<List<Smjer>> GetAll();
+
+
+
+        Task<Smjer> GetById(int id);
+
+
+
+        Task<bool> Post(Smjer smjer);
+
+
+
+        Task<bool> Put(int id, Smjer smjer);
+
+
+
+        Task<bool> DeleteById(int id);
+
+    }
+}
---
+++ Day5/Uni.Repository.Common/IStudentRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Uni.Model;
+
+namespace Uni.Repository.Common
+{
+    public interface IStudentRepository
+    {
+        Task<List<Student>> GetAll();
+
+
+        Task<Student> GetById(int id);
+
+
+        Task<bool> Post(Student smjer);
+
+
+        Task<bool> Put(int id, Student smjer);
+
+
+        Task<bool> DeleteById(int id);
+        
+    }
+}
