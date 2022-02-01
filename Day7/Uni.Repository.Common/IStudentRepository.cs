8f66264ca06913cc9b4efca9db944377f29c9bbe
Marko <64027072+mdankovic2541@users.noreply.github.com>
2022-02-01 12:44:14 +0100

    Add files via upload

---
+++ Day7/Uni.Repository.Common/IStudentRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Uni.Common;
+using Uni.Model;
+
+namespace Uni.Repository.Common
+{
+    public interface IStudentRepository
+    {
+        Task<List<Student>> GetAllAsync(StudentSort sortby, Pager sortmethod, StudentFilter filter);
+
+
+        Task<Student> GetByIdAsync(int id);
+
+
+        Task<bool> PostAsync(Student smjer);
+
+
+        Task<bool> PutAsync(int id, Student smjer);
+
+
+        Task<bool> DeleteByIdAsync(int id);
+        
+    }
+}
