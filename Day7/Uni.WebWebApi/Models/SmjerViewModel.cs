d48bbe76975b4098d07cf3eb7d4b509a154da972
Marko <64027072+mdankovic2541@users.noreply.github.com>
2022-02-01 12:45:30 +0100

    Add files via upload

---
+++ Day7/Uni.WebWebApi/Models/SmjerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Uni.WebWebApi.Models
+{
+    public class SmjerViewModel
+    {
+        public string Naziv { get; set; }
+
+        public List<StudentViewModel> Studenti { get; set; }
+    }
+}
