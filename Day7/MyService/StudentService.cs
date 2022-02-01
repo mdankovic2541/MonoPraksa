bf6a46c891fa49778a1291817df02bd6202facec
Marko <64027072+mdankovic2541@users.noreply.github.com>
2022-02-01 12:42:38 +0100

    Add files via upload

---
+++ Day7/MyService/StudentService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Uni.Common;
+using Uni.Model;
+using Uni.Repository;
+using Uni.Repository.Common;
+using Uni.Service.Common;
+
+namespace Uni.Service
+{
+    public class StudentService : IStudentService
+    {
+       
+
+        public StudentService()
+        {
+        }
+
+        protected IStudentRepository Repository = new StudentRepository();
+        protected ISmjerRepository SmjerRepository = new SmjerRepository();
+
+        public async Task<List<Student>> GetAllAsync(StudentSort sort, Pager pager, StudentFilter filter)
+        {
+            List<Smjer> smjerovi;
+            List<Student> studenti;
+            List<Student> studentiNovi = new List<Student>();
+            SmjerSort sorterSmjer = new SmjerSort("","");
+            SmjerFilter smjerFilter = new SmjerFilter("");
+            
+            smjerovi = await SmjerRepository.GetAllAsync(sorterSmjer,pager,smjerFilter);
+            studenti = await Repository.GetAllAsync(sort,pager,filter);
+            foreach (Student student in studenti)
+            {
+                foreach (Smjer sm in smjerovi)
+                {
+                    if (student.SmjerId == sm.Id)
+                    {
+                        Student stud = new Student();
+                        stud.Smjer = new Smjer();
+                        stud.FirstName = student.FirstName;
+                        stud.LastName = student.LastName;
+                        stud.IdNumber = student.IdNumber;
+                        stud.Smjer= sm;                       
+                        stud.SmjerId = sm.Id;
+                        studentiNovi.Add(stud);
+                    }
+                }
+            }
+
+
+
+            return studentiNovi;
+        }
+
+
+        public async Task<Student> GetByIdAsync(int id)
+        {
+
+            Student stud;
+            Smjer smjer;         
+            stud = await Repository.GetByIdAsync(id);
+            smjer = await SmjerRepository.GetByIdAsync(stud.SmjerId);
+            
+            Student student = new Student();
+            student.Smjer = smjer;
+            student.FirstName = stud.FirstName;
+            student.LastName = stud.LastName;
+            student.IdNumber = stud.IdNumber;
+            student.SmjerId = smjer.Id;
+
+            return student;
+        }
+
+
+        public async Task<bool> PostAsync(Student smjer)
+        {
+            return await Repository.PostAsync(smjer);
+        }
+
+
+
+        public async Task<bool> PutAsync(int id, Student smjer)
+        {
+            return await Repository.PutAsync(id, smjer);
+        }
+
+
+        public async Task<bool> DeleteByIdAsync(int id)
+        {
+            return await Repository.DeleteByIdAsync(id);
+        }
+
+    }
+}
