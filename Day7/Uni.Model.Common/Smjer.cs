06cf5cb557a28b9200abab7b4900441229123955
Marko <64027072+mdankovic2541@users.noreply.github.com>
2022-02-01 12:43:05 +0100

    Add files via upload

---
+++ Day7/Uni.Model.Common/Smjer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uni.Model.Common;
+
+namespace Uni.Model
+{
+    public class Smjer : ISmjer
+    {
+        public int Id { get; set; }
+
+        public string Naziv { get; set; }
+
+        public List<Student> Studenti { get; set; }
+    }
+}
