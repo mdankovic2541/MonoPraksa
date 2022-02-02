1b2e114442834f4106965452fa9395cc0129411d
Marko <64027072+mdankovic2541@users.noreply.github.com>
2022-02-02 15:22:28 +0100

    Add files via upload

---
+++ Day8/Uni.Model.Common/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uni.Model.Common;
+
+namespace Uni.Model
+{
+    public class Student : IStudent
+    {
+        public int Id { get ; set ; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string IdNumber { get; set ; }
+
+        public int SmjerId { get; set ; }
+
+        public Smjer Smjer { get; set; }
+
+        public static implicit operator Student(Smjer v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
