71aaa8456d65e964b5a4363904117474e1ef0d80
Marko <64027072+mdankovic2541@users.noreply.github.com>
2022-02-02 15:19:11 +0100

    Add files via upload

---
+++ Day8/Uni.Service.Common/ISmjerService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Uni.Common;
+using Uni.Model;
+
+namespace Uni.Service.Common
+{
+    public interface ISmjerService
+    {
+        Task<List<Smjer>> GetAllAsync(SmjerSort sort, Pager pager, SmjerFilter filter);
+
+
+
+        Task<Smjer> GetByIdAsync(int id);
+
+
+
+        Task<bool> PostAsync(Smjer smjer);
+
+
+
+        Task<bool> PutAsync(int id, Smjer smjer);
+
+
+
+        Task<bool> DeleteByIdAsync(int id);
+        
+    }
+}
