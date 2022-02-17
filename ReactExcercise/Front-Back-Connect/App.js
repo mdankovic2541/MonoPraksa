7b327add9f31a3eb591369d69606a8f3daa61b02
Marko <64027072+mdankovic2541@users.noreply.github.com>
2022-02-17 15:14:07 +0100

    Add files via upload

---
+++ ReactExcercise/Front-Back-Connect/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
+import Motor from './Motor'
+
+
+function App() {
+ 
+  return (
+    <div className="App container">
+      <Motor />
+    </div>
+  );
+}
+
+export default App;
